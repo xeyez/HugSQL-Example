d8106427255b9e0247ea4f255b2c1b0cce0457e5
Joohyuk Lee <joohyuk@miraeclimate.com>
2017-11-29 16:02:12 +0900

    first commit

---
+++ test/hugsql_example/core_test.clj
@@ -0,0 +1,7 @@
+(ns hugsql-example.core-test
+  (:require [clojure.test :refer :all]
+            [hugsql-example.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
